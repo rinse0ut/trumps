04d7976e8df9e91a5c9b8cbdccf523365b6dc4d9
Dave Thomson <djthomson@gmail.com>
2020-10-16 13:46:45 +0100

    Updates

---
+++ precache-manifest.e4a03483bdba56195a6fe37ddba78dc5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4965028c4713fff34bd4ed4a5b74782e",
+    "url": "/trumps/index.html"
+  },
+  {
+    "revision": "a272800077704fdf09d4",
+    "url": "/trumps/static/css/2.6a4efc55.chunk.css"
+  },
+  {
+    "revision": "492510f694b594731852",
+    "url": "/trumps/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a272800077704fdf09d4",
+    "url": "/trumps/static/js/2.82c3db82.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/trumps/static/js/2.82c3db82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "492510f694b594731852",
+    "url": "/trumps/static/js/main.9c8dc7a9.chunk.js"
+  },
+  {
+    "revision": "95d82a9caed866a55fc7",
+    "url": "/trumps/static/js/runtime-main.115d4ff9.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/trumps/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/trumps/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/trumps/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/trumps/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/trumps/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/trumps/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/trumps/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/trumps/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/trumps/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/trumps/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/trumps/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/trumps/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/trumps/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/trumps/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/trumps/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/trumps/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
