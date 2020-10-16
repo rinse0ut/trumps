852236b515c145ceae4dfb29bb33647d6867334d
Dave Thomson <djthomson@gmail.com>
2020-10-16 13:39:33 +0100

    Add turn delay

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Container, Card, Table, Icon } from 'semantic-ui-react';
+import { Container, Label, Table, Icon } from 'semantic-ui-react';
 
 type CategoryType = 'Age' | 'Height' | 'Bench' | 'Banter';
 
@@ -17,11 +17,16 @@ const DATA: CardType[] = [
   { name: 'Sunny', values: [37, 188, 50, 100] },
 ];
 
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function useGame() {
   const [currentPlayer, setCurrentPlayer] = useState<1 | 2>(1);
   const [player1Stack, setPlayer1Stack] = useState<CardType[]>([]);
   const [player2Stack, setPlayer2Stack] = useState<CardType[]>([]);
   const [drawnStack, setDrawnStack] = useState<CardType[]>([]);
+  const [showCard, setShowCard] = useState<boolean>(false);
 
   // Shuffle and setup each player decks
   useEffect(() => {
@@ -36,11 +41,19 @@ function useGame() {
   }, [])
 
   const handleTurn = useCallback((categoryIndex: number) => {
+    setShowCard(true);
+    delay(3000).then(() => { 
+      alert('Next turn?')
+      setShowCard(false);
+      handleCards(categoryIndex)
+    });
+  }, [player1Stack, player2Stack, drawnStack]);  
+    
+  const handleCards = useCallback((categoryIndex: number) => {
     const player1Card = player1Stack[0];
     const player2Card = player2Stack[0];
     const player1Value = player1Card['values'][categoryIndex];
     const player2Value = player2Card['values'][categoryIndex];
-    console.log('PLAYER1VALUE', player1Value);
     const _player1Stack = player1Stack.slice(1);
     const _player2Stack = player2Stack.slice(1);
 
@@ -59,15 +72,14 @@ function useGame() {
       setPlayer2Stack([..._player2Stack]);
       setDrawnStack([...drawnStack, player2Card, player1Card]);
     }
-    console.log('SELECTED VALUES', player1Value, player2Value);
   }, [player1Stack, player2Stack, drawnStack])
 
-  return { currentPlayer, player1Stack, player2Stack, drawnStack, handleTurn }
+  return { currentPlayer, showCard, player1Stack, player2Stack, drawnStack, handleTurn }
 }
 
 function App() {
 
-  const { currentPlayer, player1Stack, player2Stack, drawnStack, handleTurn } = useGame();
+  const { currentPlayer, showCard, player1Stack, player2Stack, drawnStack, handleTurn } = useGame();
   console.log('1UP STACK', player1Stack);
   console.log('2UP STACK', player2Stack);
   console.log('DRAWN STACK', drawnStack);
@@ -82,10 +94,10 @@ function App() {
   }
   return (
     <Container>
-      <h1>1up Stack ({player1Stack.length} Cards)</h1>
-      {currentPlayer === 1 ? <CardComponent categories={CATEGORIES} onTurn={handleTurn} {...player1Stack[0]} /> : null}
-      <h1>2up Stack ({player2Stack.length} Cards)</h1>
-      {currentPlayer === 2 ? <CardComponent categories={CATEGORIES} onTurn={handleTurn} {...player2Stack[0]} /> : null}
+      <h1>Player 1 ({player1Stack.length} Cards)</h1>
+      {currentPlayer === 1 || showCard ? <CardComponent categories={CATEGORIES} onTurn={handleTurn} {...player1Stack[0]} /> : null}
+      <h1>Player 2 ({player2Stack.length} Cards)</h1>
+      {currentPlayer === 2 || showCard ? <CardComponent categories={CATEGORIES} onTurn={handleTurn} {...player2Stack[0]} /> : null}
     </Container>
   );
 }
@@ -98,23 +110,22 @@ type CardPropsType = CardType & {
 function CardComponent(props: CardPropsType) {
   const { name, values, onTurn } = props;
   return (
-    <Card>
-      <Card.Content>
-        <Card.Header>{name}</Card.Header>
-      </Card.Content>
-      <Card.Content extra>
-        <Table celled selectable size="small">
-          <Table.Body>
-            {CATEGORIES.map((category, i) => (
-              <Table.Row onClick={() => onTurn(i)}>
-                <Table.Cell>{category}</Table.Cell>
-                <Table.Cell>{values[i]}</Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </Card.Content>
-    </Card>
+    <Table celled selectable unstackable size="small">
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell colSpan='3'>{name.toUpperCase()}</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {CATEGORIES.map((category, i) => (
+          <Table.Row onClick={() => onTurn(i)}>
+            <Table.Cell>{category}</Table.Cell>
+            <Table.Cell>{values[i]}</Table.Cell>
+            <Table.Cell></Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
   );
 }
 
