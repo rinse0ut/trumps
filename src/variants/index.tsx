53517647cfe1229dce07a4663488ee6fc79a270b
Dave Thomson <djthomson@gmail.com>
2020-10-19 19:10:22 +0100

    Add more  variants

--- src/variants/index.tsx
+++ src/variants/index.tsx
@@ -1,9 +1,13 @@
 import pilot from './pilot';
 import classic from './classic';
 import drink from './drink';
+import gaytimes from './gaytimes';
+import player from './player';
 
 export default {
   pilot,
   classic,
-  drink
+  drink,
+  player,
+  gaytimes
 }
