a061d34e9e595037128da07567d83eba539f861a
Dave Thomson <djthomson@gmail.com>
2020-11-27 21:36:59 +0000

    Updates

---
+++ precache-manifest.a21e3e23d52a5a6a377fea2117be15bb.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b157f0ff88c415b01c68087b5b31554",
+    "url": "/trump/index.html"
+  },
+  {
+    "revision": "a159132a54077d70cfd3",
+    "url": "/trump/static/css/2.eb73ca23.chunk.css"
+  },
+  {
+    "revision": "6e9b5f244b2b69675018",
+    "url": "/trump/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a159132a54077d70cfd3",
+    "url": "/trump/static/js/2.0039be4a.chunk.js"
+  },
+  {
+    "revision": "661fd5aedd499bc9f79c3d39c1a3b9b0",
+    "url": "/trump/static/js/2.0039be4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e9b5f244b2b69675018",
+    "url": "/trump/static/js/main.48bcc571.chunk.js"
+  },
+  {
+    "revision": "df4f0c204b2c5fe1039c",
+    "url": "/trump/static/js/runtime-main.32bc106c.js"
+  },
+  {
+    "revision": "2fe8faf80d35c9927bff9be28fa479b2",
+    "url": "/trump/static/media/ant.2fe8faf8.jpeg"
+  },
+  {
+    "revision": "6cba428d232580549ada0f9d75587c99",
+    "url": "/trump/static/media/ben.6cba428d.jpeg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/trump/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/trump/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/trump/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/trump/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/trump/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "abf052b928df1ddd4ccabbd73a8ac59c",
+    "url": "/trump/static/media/dan.abf052b9.jpeg"
+  },
+  {
+    "revision": "2cfccad8fbe6e4d37b09abdae3e12a37",
+    "url": "/trump/static/media/didun.2cfccad8.jpeg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/trump/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "39b9be8622b0baf0bf6563365a92536c",
+    "url": "/trump/static/media/grant.39b9be86.jpeg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/trump/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/trump/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/trump/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/trump/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/trump/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "006e457e92bd828e4299c10551fdb2c8",
+    "url": "/trump/static/media/mike.006e457e.jpeg"
+  },
+  {
+    "revision": "9c9ff2b1ef51989202002088d734b446",
+    "url": "/trump/static/media/morgan.9c9ff2b1.jpeg"
+  },
+  {
+    "revision": "c153051e4fdfd56f20d2ef87eac7c4fb",
+    "url": "/trump/static/media/nick.c153051e.jpeg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/trump/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/trump/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/trump/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/trump/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/trump/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "2064c0f2d74efbc2f8091c2580820da8",
+    "url": "/trump/static/media/pearce.2064c0f2.jpeg"
+  },
+  {
+    "revision": "5110024b737f237f181c5d2b59c48b94",
+    "url": "/trump/static/media/rob.5110024b.jpeg"
+  },
+  {
+    "revision": "d02204f4a784bb0ec98b0a01cdb9b8d1",
+    "url": "/trump/static/media/scouse.d02204f4.jpeg"
+  },
+  {
+    "revision": "965acec987504c2e984cfc33ab5767d4",
+    "url": "/trump/static/media/stevooo.965acec9.jpeg"
+  },
+  {
+    "revision": "c00e8603fc6654cac2ee95571f2045ed",
+    "url": "/trump/static/media/sunny.c00e8603.jpeg"
+  },
+  {
+    "revision": "bb7ff1a72dc667990374d2a1dd9e4708",
+    "url": "/trump/static/media/vinnie.bb7ff1a7.jpeg"
+  },
+  {
+    "revision": "32aef3bdef7d31f959843f316f448367",
+    "url": "/trump/static/media/will.32aef3bd.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5064ea847be858f1330a61c4f17c487.js"
+  "/trump/precache-manifest.a21e3e23d52a5a6a377fea2117be15bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trump/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
